03ec390e24c12d2780c6fb727b24c54d5e5f06cc
nicolasguanzini <nicolasguanzini@hotmail.com>
2022-06-15 00:33:28 -0300

    Finalizo el esqueleto con webpack development y watch

---
+++ weather-app/src/index.js
@@ -0,0 +1,18 @@
+import { printMe } from './print.js';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+  
+    element.innerHTML = "Hello webpack";
+
+    btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe("Nicolas");
+
+  element.appendChild(btn);
+  
+    return element;
+  }
+  
+
+  document.body.appendChild(component());
---
+++ weather-app/src/print.js
@@ -0,0 +1,5 @@
+function printMe(nombre) {
+    console.log(`Mi nombre es ${nombre}`);
+  }
+
+export { printMe };

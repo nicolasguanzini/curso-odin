4c2b8072a4d9e5cbe2b9527823477e36b60a4831
nicolasguanzini <nicolasguanzini@hotmail.com>
2022-06-17 20:01:42 -0300

    creando aplicacion todo

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./components/header.js";
+
+
+function Proyecto(nombre, todos) {
+  this.nombre = nombre;
+  this.todos = todos;
+}
+
+function Todo(descripcion, completado) {
+  this.descripcion = descripcion;
+  this.completado = completado;
+}
 
 function App() {
+  const [proyectos, setProyectos] = React.useState([]);
+  const [proyectoActual, setProyectoActual] = React.useState(null);
+  const [todos, setTodos] = React.useState([]);
+  const [todoActual, setTodoActual] = React.useState(null);
+
+  const agregarProyecto = (nombre) => {
+    const nuevoProyecto = new Proyecto(nombre, []);
+    setProyectos([...proyectos, nuevoProyecto]);
+    setProyectoActual(nuevoProyecto);
+  }
+
+  const agregarTodo = (descripcion) => {
+    const nuevoTodo = new Todo(descripcion, false);
+    setTodos([...todos, nuevoTodo]);
+    setTodoActual(nuevoTodo);
+  }
+
+  const eliminarProyecto = (proyecto) => {
+    setProyectos(proyectos.filter(p => p.nombre !== proyecto.nombre));
+    
+  }
+
+  const eliminarTodo = (todo) => {
+    setTodos(todos.filter(t => t.descripcion !== todo.descripcion));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {(todos[0] === undefined ? <h2>No hay todos</h2> : 
+      todos.map((todo, index) => {
+        return(
+          <div key={index}>{todo.descripcion}</div>
+        )
+      }))}
+      <button onClick={()=> agregarTodo("hola")}>Agregar todo</button>
     </div>
   );
 }
---
+++ todo-list/src/components/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+    return (
+      <div className="Header">
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+  
+  export default Header;

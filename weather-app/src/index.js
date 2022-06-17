e51658b91fc64484b4bfe8c6f967fc553597fc9c
nicolasguanzini <nicolasguanzini@hotmail.com>
2022-06-16 21:33:55 -0300

    Cambio API Key

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -2,7 +2,7 @@ import { pantalla } from "./components/pantalla.js";
 import { clima } from "./components/clima.js";
 
 async function getData(ciudad){
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`;
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=70e896396874524da7286624eac2e8e1`;
 try {
     const response = await fetch(endpoint, { mode: "cors" });
     if (!response.ok) throw new Error(`City not found`);
@@ -19,11 +19,9 @@ document.body.appendChild(pantalla());
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  console.log(formData.get("ciudad"));
 
   getData(formData.get("ciudad"))
   .then((data) => {
-    console.log(data);
     document.body.appendChild(clima(data));
   })
   .catch((error)=> console.error(error));

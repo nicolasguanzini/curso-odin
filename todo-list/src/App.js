36c7bc1103c7df7c22c055b671d15af4125b1872
nicolasguanzini <nicolasguanzini@hotmail.com>
2022-06-19 22:15:53 -0300

    Update App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -77,6 +77,7 @@ function App() {
 
   return (
     <div className="App">
+      
       <Addproject agregarProyecto={agregarProyecto} visibilidadAddProyecto={visibilidadAddProyecto} setVisibilidadAddProyecto={setVisibilidadAddProyecto}/>
 
       <Header />
